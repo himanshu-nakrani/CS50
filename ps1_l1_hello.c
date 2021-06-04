77c8a5a3f7c3c126ab5cca03e3d2a55dde283f1d
himanshu-nakrani <82092249+himanshu-nakrani@users.noreply.github.com>
2021-06-04 22:16:41 +0530

    Create ps1_l1_hello.c

---
+++ ps1_l1_hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main()
+{
+
+    string a = get_string("What is your name?\n");     //get name from user
+    printf("hello, %s\n", a);                          //prints hello, name
+
+}
+
